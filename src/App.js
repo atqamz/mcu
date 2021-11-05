9123f7a2ef678f7bcc7f647aa040f552e3e00f46
Atqa Munzir <atqamz@gmail.com>
2021-11-06 01:28:16 +0700

    First Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+// Component
+import Navbar from "./components/Navbar.com";
+import Footer from "./components/Footer.com";
+
+// Pages
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+      <BrowserRouter>
+        <Route exact path='/'>
+          <Home search={searchTerm} />
+        </Route>
+        <Route path='/movie/:id'>
+          <Detail />
+        </Route>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import * as MatIco from "@material-ui/icons";
+import data from "../dummyData";
+
+export default function Home(props) {
+  let searchTerm = props.search;
+
+  const [wishlist, setWishlist] = useState([]);
+
+  function handleClick(id) {
+    setWishlist([...wishlist, { movieId: id }]);
+    console.log(wishlist);
+  }
+
+  function handleDelete(id) {
+    setWishlist(wishlist.filter((data) => data.movieId !== id));
+    console.log(wishlist);
+  }
+
+  return (
+    <>
+      <div className='row container-fluid py-3'>
+        <h1 className='display-5 p-5 fw-bold col-md-8 d-flex align-items-center'>
+          Search for Your favorite MCU movies
+        </h1>
+        <img
+          className='col-md-4'
+          src='./assets/marvel-cinematic-universe.png'
+          alt='Marvel Cinematic Universe'
+          width='350'
+        />
+      </div>
+
+      <div className='row container-fluid p-5'>
+        <h1>Wishlist</h1>
+        {data.results
+          .filter((movie) => {
+            return wishlist.find((data) => {
+              return data.movieId === movie.id;
+            });
+          })
+          .map((movie) => {
+            return (
+              <div className='col-md-2 movie-list d-flex flex-column mb-4'>
+                <button className='btn btn-danger' onClick={() => handleDelete(movie.id)}>
+                  <MatIco.Delete />
+                </button>
+                <a href={`/movie/${movie.id}`} key={movie.id} id={movie.id}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                    alt={movie.title}
+                    className='img-fluid'
+                  />
+                </a>
+                <p class='text-center p-3'>{movie.title}</p>
+              </div>
+            );
+          })}
+      </div>
+
+      <div className='row container-fluid p-5'>
+        <h1>Movie List</h1>
+        {data.results
+          .filter((movie) => {
+            if (searchTerm === "") {
+              return movie;
+            } else if (
+              movie.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+            ) {
+              return movie;
+            }
+          })
+          .map((movie) => {
+            return (
+              <div className='col-md-2 movie-list d-flex flex-column mb-4'>
+                <button className='btn btn-danger' onClick={() => handleClick(movie.id)}>
+                  <MatIco.AddCircle />
+                </button>
+                <a href={`/movie/${movie.id}`} key={movie.id} id={movie.id}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                    alt={movie.title}
+                    className='img-fluid'
+                  />
+                </a>
+                <p class='text-center p-3'>{movie.title}</p>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+}
